13742d6f57ad68d3925451cfeee56cbacb9c2b78
lethosor <lethosor@gmail.com>
2025-08-09 04:17:50 -0400

    fix some issues

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from gitdb import IStream
 
 @dataclass
 class GitFileContents:
-    data: bytes
+    data: str
     mode: str = "100644"  # default: regular file
 
 
@@ -59,12 +59,16 @@ def commit_replace_subdir(
         for path, file_contents in files.items():
             full_path = subdir_prefix + to_git_path(path)
 
-            stream = IStream(
-                b"blob", len(file_contents.data), io.BytesIO(file_contents.data)
-            )
-            blob_sha = repo.odb.store(stream).hexsha
+            data_bytes = file_contents.data.encode("utf-8")
+            stream = IStream(b"blob", len(data_bytes), io.BytesIO(data_bytes))
+            blob_sha = repo.odb.store(stream).hexsha.decode()
             repo.git.update_index(
-                "--add", "--cacheinfo", file_contents.mode, blob_sha, full_path, env=env
+                "--add",
+                "--cacheinfo",
+                str(file_contents.mode),
+                blob_sha,
+                full_path,
+                env=env,
             )
 
         tree_sha = repo.git.write_tree(env=env)
@@ -79,9 +83,9 @@ def commit_replace_subdir(
             "GIT_COMMITTER_EMAIL": c_email,
         }
         if author_date:
-            commit_env["GIT_AUTHOR_DATE"] = author_date
+            commit_env["GIT_AUTHOR_DATE"] = str(int(author_date.timestamp()))
         if committer_date:
-            commit_env["GIT_COMMITTER_DATE"] = committer_date
+            commit_env["GIT_COMMITTER_DATE"] = str(int(committer_date.timestamp()))
 
         new_commit_sha = repo.git.commit_tree(
             tree_sha, "-p", base.hexsha, "-m", message, env=commit_env
@@ -97,8 +101,9 @@ if __name__ == "__main__":
         subdir="data",
         files={
             "a": GitFileContents("contents a"),
-            "b": GitFileContents("b", "contents b"),
+            "b": GitFileContents("contents b"),
         },
         message="test commit 1",
+        author=("me", "my@email"),
         author_date=datetime.datetime.now(),
     )
