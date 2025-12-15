d033e848a2a867030c2a8943d4b05156866b217e
lethosor <lethosor@gmail.com>
2025-12-15 01:38:20 -0500

    frontend skeleton

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Styles as BootstrapStyles } from '@sveltestrap/sveltestrap';
+
+  let { children } = $props();
+</script>
+
+<BootstrapStyles />
+
+{@render children()}
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from 'svelte';
+  import { Container, Spinner } from '@sveltestrap/sveltestrap';
+
+  onMount(() => {
+    const queryStr = location.search.replace(/^\?/, '').replace(/^\//, '');
+    if (!queryStr) {
+      location.search = '/2026/frc';
+    }
+  });
+</script>
+
+<Container class="mt-2 d-flex align-items-center justify-content-center h-100">
+  <Spinner />
+</Container>
